ab99fcf0a7bea67a6586b79b8d8e1e469e15349c
Bryce Roethel <me@bryceroethel.com>
2025-02-15 19:51:31 -0500

    ROBOT 🗣️🔥‼️‼️

--- dashboard/src/constants.ts
+++
@@ -1,8 +0,0 @@
-// Default NT URI.
-// export const DEFAULT_URI = `roborio-340-frc.local`;
-export const DEFAULT_URI = `localhost`; // Use for simulation
-
-// Field size constants.
-// Set these values to the field's size in meters.
-export const FIELD_WIDTH = 16.54175;
-export const FIELD_HEIGHT = 8.21055;
--- src/main/java/org/team340/robot/subsystems/Swerve.java
+++ src/main/java/org/team340/robot/subsystems/Swerve.java
@@ -1,108 +1,120 @@
 package org.team340.robot.subsystems;
 
-import static edu.wpi.first.wpilibj2.command.Commands.*;
-
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.ADIS16470_IMU.CalibrationTime;
+import edu.wpi.first.wpilibj.ADIS16470_IMU.IMUAxis;
+import edu.wpi.first.wpilibj.SPI.Port;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import java.util.function.Supplier;
-import org.team340.lib.swerve.SwerveBase;
-import org.team340.lib.util.Math2;
-import org.team340.robot.Constants.SwerveConstants;
+import java.util.function.DoubleSupplier;
+import org.team340.lib.swerve.Perspective;
+import org.team340.lib.swerve.SwerveAPI;
+import org.team340.lib.swerve.config.SwerveConfig;
+import org.team340.lib.swerve.config.SwerveModuleConfig;
+import org.team340.lib.swerve.hardware.SwerveEncoders;
+import org.team340.lib.swerve.hardware.SwerveIMUs;
+import org.team340.lib.swerve.hardware.SwerveMotors;
+import org.team340.lib.util.command.GRRSubsystem;
+import org.team340.robot.Constants;
+import org.team340.robot.Constants.RobotMap;
 
 /**
- * The swerve subsystem.
+ * The robot's swerve drivetrain.
  */
-public class Swerve extends SwerveBase {
+@Logged
+public final class Swerve extends GRRSubsystem {
 
-    private final ProfiledPIDController rotController = new ProfiledPIDController(
-        SwerveConstants.ROT_PID.p(),
-        SwerveConstants.ROT_PID.i(),
-        SwerveConstants.ROT_PID.d(),
-        SwerveConstants.ROT_CONSTRAINTS
-    );
+    private static final SwerveModuleConfig kFrontLeft = new SwerveModuleConfig()
+        .setName("frontLeft")
+        .setLocation(0.27305, 0.27305)
+        .setMoveMotor(SwerveMotors.sparkMax(RobotMap.kFlMove, MotorType.kBrushless, true))
+        .setTurnMotor(SwerveMotors.sparkMax(RobotMap.kFlTurn, MotorType.kBrushless, true))
+        .setEncoder(SwerveEncoders.sparkMaxEncoder(0.856, false));
 
-    /**
-     * Create the swerve subsystem.
-     */
+    private static final SwerveModuleConfig kFrontRight = new SwerveModuleConfig()
+        .setName("frontRight")
+        .setLocation(0.27305, -0.27305)
+        .setMoveMotor(SwerveMotors.sparkMax(RobotMap.kFrMove, MotorType.kBrushless, true))
+        .setTurnMotor(SwerveMotors.sparkMax(RobotMap.kFrTurn, MotorType.kBrushless, true))
+        .setEncoder(SwerveEncoders.sparkMaxEncoder(0.463, false));
+
+    private static final SwerveModuleConfig kBackLeft = new SwerveModuleConfig()
+        .setName("backLeft")
+        .setLocation(-0.27305, 0.27305)
+        .setMoveMotor(SwerveMotors.sparkMax(RobotMap.kBlMove, MotorType.kBrushless, true))
+        .setTurnMotor(SwerveMotors.sparkMax(RobotMap.kBlTurn, MotorType.kBrushless, true))
+        .setEncoder(SwerveEncoders.sparkMaxEncoder(0.802, false));
+
+    private static final SwerveModuleConfig kBackRight = new SwerveModuleConfig()
+        .setName("backRight")
+        .setLocation(-0.27305, -0.27305)
+        .setMoveMotor(SwerveMotors.sparkMax(RobotMap.kBrMove, MotorType.kBrushless, true))
+        .setTurnMotor(SwerveMotors.sparkMax(RobotMap.kBrTurn, MotorType.kBrushless, true))
+        .setEncoder(SwerveEncoders.sparkMaxEncoder(0.170, false));
+
+    private static final SwerveConfig kConfig = new SwerveConfig()
+        .setTimings(TimedRobot.kDefaultPeriod, TimedRobot.kDefaultPeriod, 0.04)
+        .setMovePID(0.01, 0.0, 0.0)
+        .setMoveFF(0.05, 0.127)
+        .setTurnPID(10.0, 0.0, 0.2)
+        .setBrakeMode(false, true)
+        .setLimits(4.2, 13.5, 11.5, 28.0)
+        .setDriverProfile(4.0, 1.0, 0.15, 4.2, 2.0, 0.1)
+        .setPowerProperties(Constants.kVoltage, 80.0, 60.0)
+        .setMechanicalProperties(7.13, 13.71, 0.0, Units.inchesToMeters(4.0))
+        .setOdometryStd(0.1, 0.1, 0.1)
+        .setIMU(SwerveIMUs.adis16470(IMUAxis.kZ, IMUAxis.kX, IMUAxis.kY, Port.kOnboardCS0, CalibrationTime._4s))
+        .setModules(kFrontLeft, kFrontRight, kBackLeft, kBackRight);
+
+    private final SwerveAPI api;
+
     public Swerve() {
-        super("Swerve Drive", SwerveConstants.CONFIG);
-        rotController.setIZone(SwerveConstants.ROT_PID.iZone());
-        rotController.enableContinuousInput(-Math.PI, Math.PI);
+        api = new SwerveAPI(kConfig);
+        api.enableTunables("swerve/api");
     }
 
     @Override
     public void periodic() {
-        updateOdometry();
+        api.refresh();
     }
 
     /**
-     * Zeroes the IMU to a specified yaw.
+     * Tares the rotation of the robot. Useful for
+     * fixing an out of sync or drifting IMU.
      */
-    public Command zeroIMU(Rotation2d yaw) {
-        return runOnce(() -> imu.setZero(yaw)).withName("swerve.zero(" + yaw.toString() + ")");
+    public Command tareRotation() {
+        return commandBuilder("Swerve.tareRotation()")
+            .onInitialize(() -> api.tareRotation(Perspective.kOperator))
+            .isFinished(true)
+            .ignoringDisable(true);
     }
 
     /**
-     * Drives the robot as a percent of its max velocity (inputs are from {@code -1.0} to {@code 1.0}).
-     * @param x X speed.
-     * @param y Y speed.
-     * @param rot Rotational speed.
-     * @param fieldRelative If the robot should drive field relative.
+     * Drives the robot using driver input.
+     * @param x The X value from the driver's joystick.
+     * @param y The Y value from the driver's joystick.
+     * @param angular The CCW+ angular speed to apply, from {@code [-1.0, 1.0]}.
      */
-    public Command drive(Supplier<Double> x, Supplier<Double> y, Supplier<Double> rot, boolean fieldRelative) {
-        return commandBuilder("swerve.drive()").onExecute(() -> drive(x.get(), y.get(), rot.get(), fieldRelative));
+    public Command drive(DoubleSupplier x, DoubleSupplier y, DoubleSupplier angular) {
+        return commandBuilder("Swerve.drive()").onExecute(() ->
+            api.applyDriverInput(
+                x.getAsDouble(),
+                y.getAsDouble(),
+                angular.getAsDouble(),
+                Perspective.kOperator,
+                true,
+                true
+            )
+        );
     }
 
     /**
-     * Drives the robot using percents of its calculated max velocity while locked at a field relative angle.
-     * @param x X speed.
-     * @param y Y speed.
-     * @param angle The desired field relative angle to point at in radians.
+     * Drives the modules to stop the robot from moving.
+     * @param lock If the wheels should be driven to an X formation to stop the robot from being pushed.
      */
-    public Command driveAngle(Supplier<Double> x, Supplier<Double> y, double angle) {
-        return commandBuilder("swerve.driveAngle(" + Math2.toFixed(angle) + ")")
-            .onInitialize(() -> rotController.reset(getPosition().getRotation().getRadians(), getVelocity(true).omegaRadiansPerSecond))
-            .onExecute(() -> driveAngle(x.get(), y.get(), angle, rotController));
-    }
-
-    /**
-     * Drives the robot while snapping to facing up or down the field, whichever is closer.
-     * Ends when the robot is at the determined angle.
-     * @param x X speed.
-     * @param y Y speed.
-     */
-    public Command driveSnap180(Supplier<Double> x, Supplier<Double> y) {
-        return either(
-            driveAngle(x, y, 0.0),
-            driveAngle(x, y, Math.PI),
-            () -> Math.abs(MathUtil.angleModulus(imu.getYaw().getRadians())) < Math2.HALF_PI
-        )
-            .withName("swerve.driveSnap180()");
-    }
-
-    /**
-     * Drives the modules into an X formation to prevent the robot from moving.
-     */
-    public Command lock() {
-        return commandBuilder("swerve.lock()").onExecute(this::lockWheels);
-    }
-
-    /**
-     * Runs a SysId quasistatic test.
-     * @param direction The direction to run the test in.
-     */
-    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
-        return sysIdRoutine.quasistatic(direction);
-    }
-
-    /**
-     * Runs a SysId dynamic test.
-     * @param direction The direction to run the test in.
-     */
-    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
-        return sysIdRoutine.dynamic(direction);
+    public Command stop(boolean lock) {
+        return commandBuilder("Swerve.stop(" + lock + ")").onExecute(() -> api.applyStop(lock));
     }
 }
